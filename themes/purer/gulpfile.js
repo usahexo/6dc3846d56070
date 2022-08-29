e52def9d7fc6e8723bff8285692082bf87f119f8
sfj297092319 <110655703+sfj297092319@users.noreply.github.com>
2022-08-29 20:31:25 +0800

    a

---
+++ themes/purer/gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglifyES = require('gulp-uglify-es').default;
+const postcss = require('gulp-postcss');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('css', () => gulp
+  .src('src/**/*.css')
+// eslint-disable-next-line global-require,import/no-unresolved
+  .pipe(
+    postcss([
+      require('postcss-import'),
+      require('tailwindcss/nesting'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]),
+  )
+  .pipe(cleanCSS())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('source')));
+
+gulp.task('dom-event', () => gulp
+  .src('src/js/dom-event/*.js')
+  .pipe(concat('dom-event.js'))
+  .pipe(uglifyES())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('source/js')));
+
+// gulp.task('search.js', () => gulp.src('src/js/search/*.js')
+//   .pipe(concat('search.js'))
+//   .pipe(uglifyES())
+//   .pipe(rename({ suffix: '.min' }))
+//   .pipe(gulp.dest('source/js')));
+
+gulp.task('local-search', () => gulp
+  .src('src/js/search/local-search/dist/bundle.js')
+  .pipe(rename({ basename: 'local-search', suffix: '.min' }))
+  .pipe(gulp.dest('source/js')));
+
+gulp.task('light-gallery.js', () => gulp
+  .src('src/js/light-gallery/*.js')
+  .pipe(concat('light-gallery.js'))
+  .pipe(uglifyES())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('source/js')));
+
+gulp.task('repository', () => gulp
+  .src('src/js/repository/dist/bundle.js')
+  .pipe(rename({ basename: 'repository', suffix: '.min' }))
+  .pipe(gulp.dest('source/js')));
+
+gulp.task('simple-js', () => gulp
+  .src('src/js/*.js')
+  .pipe(uglifyES())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('source/js')));
+
+gulp.task(
+  'js',
+  gulp.parallel(
+    'dom-event',
+    'light-gallery.js',
+    'simple-js',
+    'repository',
+    'local-search',
+  ),
+);
+
+gulp.task('build', gulp.parallel('css', 'js'));
+gulp.task('default', gulp.parallel('build'));
+
+gulp.task('watch', () => {
+  gulp.watch('src/**', gulp.parallel('build'));
+});
